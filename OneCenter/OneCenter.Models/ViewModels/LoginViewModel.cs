eb7d2829ef3d4076764d9db4cb39672617ab1ca6
Bryan <d58526@gmail.com>
2017-11-19 16:25:22 +0800

    Login Page

---
+++ OneCenter/OneCenter.Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneCenter.Models.ViewModels.Auth
+{
+    public class LoginViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "請輸入帳號")]
+        public string Account { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "請輸入密碼ㄖ")]
+        public string Password { get; set; }
+    }
+}
---
+++ OneCenter/OneCenter.Web/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using OneCenter.Models.ViewModels.Auth;
+
+namespace OneCenter.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
